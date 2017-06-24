206bd9e499cfa6fb69bd75f6254f3fb1358c3222
email2dorababu <email2dorababu@gmail.com>
2017-06-24 17:29:07 +0530

    commit on 24thJuly

---
+++ May2017WeekEndNoon/HashPartitioner/src/WordCountMapper.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, IntWritable, IntWritable>{
+
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+
+		StringTokenizer words=new StringTokenizer(value.toString(), " ");
+		while(words.hasMoreTokens()){
+			String word=words.nextToken();
+			context.write(new IntWritable(Integer.parseInt(word)), new IntWritable(1));
+		}
+	
+	
+	}
+	
+	
+	
+}
---
+++ May2017WeekEndNoon/HashPartitioner/src/WordCountReducer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable>{
+	@Override
+	protected void reduce(IntWritable key, Iterable<IntWritable> values,
+			Reducer<IntWritable, IntWritable, IntWritable, IntWritable>.Context context) throws IOException, InterruptedException {
+		
+		int sum=0;
+		
+		//short hand for loop
+		for (IntWritable val : values) {
+			sum = sum + val.get();
+			
+		}
+		context.write(key, new IntWritable(sum));
+	
+	
+	
+	}
+
+}
