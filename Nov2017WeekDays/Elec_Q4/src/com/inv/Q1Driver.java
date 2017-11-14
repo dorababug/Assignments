da2a5f30c108e180166eac8cfaad6333afd1daff
email2dorababu <email2dorababu@gmail.com>
2017-11-14 09:22:28 +0530

    test

---
+++ Nov2017WeekDays/Elec_Q4/src/com/inv/Q1Driver.java
@@ -0,0 +1,31 @@
+package com.inv;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Q1Driver {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf=new Configuration();
+		Job job=Job.getInstance(conf, "Q1 Driver");
+		
+		job.setJarByClass(Q1Driver.class);
+		job.setMapperClass(Q1Mapper.class);
+		//job.setNumReduceTasks(0);
+		job.setReducerClass(Q1Reducer.class);
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(Text.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		System.exit(job.waitForCompletion(true)?0:1);
+		
+	}
+}
