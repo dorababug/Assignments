68ab62625da2bd63fddaaed8864056ec2e658405
email2dorababu <email2dorababu@gmail.com>
2017-11-25 12:16:57 +0530

    test

---
+++ Nov2017WeekEnds/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("no of args="+args.length);
+		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("arguments are ="+args[i]);
+		}
+		
+	}
+
+}
