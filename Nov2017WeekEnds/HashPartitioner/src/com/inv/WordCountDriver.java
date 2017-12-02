e080bb56441652a9d609c71527860fca567b6148
email2dorababu <email2dorababu@gmail.com>
2017-12-02 12:21:52 +0530

    test

---
+++ Nov2017WeekEnds/HashPartitioner/src/com/inv/WordCountDriver.java
@@ -0,0 +1,37 @@
+package com.inv;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+
+public class WordCountDriver {
+	public static void main(String[] args) throws IllegalArgumentException, IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf=new Configuration();
+		Job job=Job.getInstance(conf, "No Mapper and Reducer");
+		
+		job.setMapperClass(WordCountMapper.class);
+		//set mapper
+		//set reducer
+		//set op key
+		//set op value
+		//job.setReducerClass(cls);
+		job.setNumReduceTasks(3);
+		job.setMapOutputKeyClass(IntWritable.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		System.exit(job.waitForCompletion(true)?0:1);
+		
+
+	}
+
+}
