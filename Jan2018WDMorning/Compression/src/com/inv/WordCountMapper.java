bde9fd2f23fb8c0c097092da63ea7b0c9e2fbd0b
email2dorababu <email2dorababu@gmail.com>
2018-02-20 09:49:58 +0530

    test

---
+++ Jan2018WDMorning/Compression/src/com/inv/WordCountMapper.java
@@ -0,0 +1,46 @@
+package com.inv;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.map.RegexMapper;
+
+public class WordCountMapper  extends Mapper<LongWritable, Text, Text, IntWritable>{
+	// source-> override implement methods
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		//Control+D to remove line in eclipse
+		
+		//(key=0,value=hi welcome to class)
+		String line=value.toString(); //convert hadoop types to java types
+		StringTokenizer words=new StringTokenizer(line," ");
+		
+		//Take one word and send (word,1) to output using contex.write
+		while(words.hasMoreTokens()){
+			String word=words.nextToken();
+			//Context can only send kout,valueout types you have defined 
+			//in Mapper signature
+			context.write(new Text(word), new IntWritable(1));
+		}
+		
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
